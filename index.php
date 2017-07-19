f4ca889aa36cc910aa65796a246ffb6518248e02
simplon <simplon@simplon-ThinkPad-T410>
2017-07-19 11:37:12 +0200

    added pdo query to SmallDoggo loop and a pdo query with argument

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ spl_autoload_register('myLoader');
 //mot clef use
 use entities\SmallDoggo;
 
-var_dump(new SmallDoggo('test', 'test', new DateTime(), false));
+//var_dump(new SmallDoggo('test', 'test', new DateTime(), false));
 
 // ça c'est pas important là
 // mysql://localhost:3306/first_db
@@ -44,6 +44,9 @@ try {
     //puis on lui donne le username et le password en deuxième et
     //troisième argument
     $db = new PDO('mysql:host=localhost;dbname=first_db', 'admin', 'simplon');
+    //On paramètre PDO afin qu'il nous renvoie les erreurs SQL
+    $db->setAttribute(PDO::ATTR_ERRMODE, 
+            PDO::ERRMODE_EXCEPTION);
     //On utilise la méthode query de notre db PDO qui 
     //attend en argument une requête SQL classique.
     //Ici, on sélectione tous les small_doggo.
@@ -61,15 +64,57 @@ try {
     $chiens = [];
     while ($ligne = $query->fetch()) {
         //créer des instances de chien à partir des lignes
-        echo '<ul>';
-        echo '<li>' . $ligne['id'] . '</li>';
-        echo '<li>' . $ligne['name'] . '</li>';
-        echo '<li>' . $ligne['race'] . '</li>';
-        echo '<li>' . $ligne['birthdate'] . '</li>';
-        echo '<li>' . $ligne['is_good'] . '</li>';
-        echo '</ul>';
+        //On fait un new SmallDoggo en lui fournissant comme
+        //argument les informations de la ligne actuelle,
+        //en oubliant pas de convertir certaines lignes dans
+        //le type voulu (le birthdate convertit en DateTime)
+        $chien = new SmallDoggo($ligne['name'],
+                $ligne['race'], 
+                new DateTime($ligne['birthdate']), 
+                $ligne['is_good'], 
+                $ligne['id']);
+        $chiens[] = $chien;
     }
-    var_dump($chiens);
+    
+    //Refaire la même chose, mais pour récupérer un small_doggo
+    //spécifique selon son id, l'id en question étant celui
+    //indiqué dans la variable $id ci dessous.
+    $id = 1;
+    /*
+     * Dans le cas de requête avec argument, plutôt que de 
+     * concaténer ceux ci dans une query, on préfèrera utiliser
+     * la méthode ->prepare, qui, contrairement à ->query, ne
+     * va pas exécuter la requête tout de suite, mais la mettre
+     * de côté jusqu'à ce qu'on lui demande explicitement de
+     * l'exécuter. Dans cette requête, on pourra mettre des 
+     * placeholder pour nos paramètre sous la forme :param auquels
+     * on pourra assigner des valeurs soit via la méthode
+     * bindParam('param', 'valeur') soit directement dans le execute
+     * sous forme d'un array associatif ->execute(['param'=>'valeur'])
+     */
+    $queryId = $db->prepare('SELECT * FROM small_doggo WHERE id=:id');
+    //On assigne au paramètre :id la valeur de notre variable $id
+    //et on lui indique que ce paramètre doit être de type INT
+    $queryId->bindParam('id', $id, PDO::PARAM_INT);
+    //On exécute la requête préparée
+    $queryId->execute();
+    //On vérifie qu'on a bien un seul résultat pour cette requête
+    if($queryId->rowCount() == 1) { //optionnel
+        //On récupère le résultat en question qu'on place dans
+        //$ligneid
+        $ligneid = $queryId->fetch();
+        //on utilise $ligneid pour créer un SmallDoggo
+        $chien = new SmallDoggo($ligneid['name'],
+                $ligneid['race'], 
+                new DateTime($ligneid['birthdate']), 
+                $ligneid['is_good'], 
+                $ligneid['id']);
+        var_dump($chien);
+    }
+    
+    
+    
+    
 } catch (PDOException $exception) {
     echo $exception->getMessage();
 }
